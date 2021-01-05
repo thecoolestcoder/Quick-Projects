476a6b43ea30f42411c1cf2ce15580a805b03c32
thecoolestcoder <65344794+thecoolestcoder@users.noreply.github.com>
2021-01-05 16:13:24 +0530

    Add files via upload

---
+++ storymaker.py
@@ -0,0 +1,13 @@
+#headings
+print('Hello! Welcome to MakeYourStory the fun helper to an Author')
+print('Now let us quickly make our story')
+#inputs
+place = input('where is your story based in: ')
+hero = input('who is your hero: ')
+villain = input('who is the villain: ')
+#maincode
+print('making your story...')
+print('once upon a time, there was a village named',place,'.In that village lived a person named',hero,' \
+      \n and there was also a person named',villain,'. ',hero,'was a very good person and was always helping others. ',villain,'always troubled others.\
+      \n One day',hero,'and ',villain,'fought with each other and ',hero,'won')
+ 
